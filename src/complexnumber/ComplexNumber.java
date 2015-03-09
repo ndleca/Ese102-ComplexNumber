347adfbf3ea9b869cc866e171c46ae99cdc3135f
ndleca <andrei523070@gmail.com>
2015-03-09 20:02:58 +0100

    Aggiunte operazioni

--- src/complexnumber/ComplexNumber.java
+++ src/complexnumber/ComplexNumber.java
@@ -1,37 +1,186 @@
 package complexnumber;
 
-public class ComplexNumber{
+public class ComplexNumber
+{
     private double re;
     private double im;
- 
-    public String formatComplexNumber(){
-        String r = this.re + "+(" + this.im + ")i";
-        return r;
-    }
-
-    public void setRectangular(double re, double im){
+        
+    /**
+     * Inzializza le variabili re e im
+     * @param re
+     * @param im
+     */
+    public void setRectangular(double re, double im)
+    {
     	this.re = re;
     	this.im = im;
     }
     
-    public void setPolar(double modulus, double argument){
-        this.im = Math.sin(argument*Math.PI/180)*modulus;
-        this.re = Math.cos(argument*Math.PI/180)*modulus;
+    /**
+     * Inizializza le variabili argument e modulus
+     * @param modulus
+     * @param argument
+     */
+    public void setPolar(double argument, double modulus)
+    {
+    	if(modulus >= 0)
+        {
+        	this.re = Math.cos(argument*Math.PI/180)*modulus;
+	        this.im = Math.sin(argument*Math.PI/180)*modulus;
+        	if(argument == 90)
+        		this.re = 0;
+        	else if(argument == 180)
+	        	this.im = 0;
+        	else if(argument == 270)
+        		this.re = 0;
+        }
+        else
+        	throw new IllegalArgumentException("il valore del modulo deve essere uguale o maggiore di 0");
     }
     
-    public double getModulus(){
-        return(Math.sqrt(Math.pow(this.re,2)+Math.pow(this.im,2)));
+    /**
+     * Somma due numeri complessi
+     * @param operand
+     * @return
+     */
+    public ComplexNumber add(ComplexNumber operand)
+    {
+    	ComplexNumber result = new ComplexNumber();
+        result.setRectangular(this.re+operand.re, this.im+operand.im);
+    	return result;
     }
     
-    public double getArgument(){
-        return((Math.atan(this.im/this.re)*180)/Math.PI);
+    /**
+     * Sottrae due numeri complessi
+     * @param operand
+     * @return
+     */
+    public ComplexNumber sub(ComplexNumber operand)
+    {
+    	ComplexNumber result = new ComplexNumber();
+    	result.setRectangular(this.re-operand.re, this.im-operand.im);
+    	return result;
     }
-      
-    public double getRe(){
+    
+    /**
+     * Moltiplica due numeri complessi
+     * @param operand
+     * @return
+     */
+    public ComplexNumber multiply(ComplexNumber operand)
+    {
+    	ComplexNumber result = new ComplexNumber();
+    	result.setPolar(this.getArgument()+operand.getArgument(), this.getModulus()*operand.getModulus());
+    	return result;
+    }
+    
+    /**
+     * Divide due numeri complessi
+     * @param operand
+     * @return
+     */
+    public ComplexNumber divide(ComplexNumber operand)
+    {
+    	ComplexNumber result = new ComplexNumber();
+    	result.setPolar(this.getArgument()-operand.getArgument(), this.getModulus()/operand.getModulus());
+    	return result;
+    }
+        
+    /**
+     * Ritorna il complesso coniugato del numero complesso
+     * @return
+     */
+    public ComplexNumber getConjugate()
+    {
+    	ComplexNumber result = new ComplexNumber();
+    	result.setRectangular(this.re, -this.im);
+    	return result;
+    }
+    
+    /**
+     * Stampa il numero complesso
+     * @return
+     */
+    public String formatComplexNumber()
+    {
+        String result = this.getRe() + "+(" + this.getIm() + ")i";
+        return result;
+    }
+    
+    /**
+     * Ritorna il modulo
+     * @return
+     */
+    public double getModulus()
+    {
+        return(Math.sqrt(Math.pow(this.re, 2)+Math.pow(this.im, 2)));
+    }
+        
+    /**
+     * Ritorna l'argomento
+     * @return
+     */
+    public double getArgument()
+    {
+    	int bugFix = 0;
+    	if(this.re < 0)
+    		bugFix = 180;
+    	return((Math.atan(this.im/this.re)*180)/Math.PI+bugFix);
+    }
+    
+    /**
+     * Ritorna re
+     * @return
+     */
+    public double getRe()
+    {
         return re;
     }
 
-    public double getIm(){
+    /**
+     * Ritorna im
+     * @return
+     */
+    public double getIm()
+    {
         return im;
     }
+    
+    /*
+     * 		1) Math.sqrt(Math.pow(this.re, 2)+Math.pow(this.im, 2))
+     * 		   Math.atan(this.im/this.re)*180)/Math.PI
+     * 		   Per re = 0 e im = 0 l'argomento non esiste
+     *         this.re = Math.cos(argument*Math.PI/180)*modulus
+	 *         this.im = Math.sin(argument*Math.PI/180)*modulus
+     * 
+     * 		2) non si puo fornire quatro funzioni setter (setRe, setIm, setModulus, setArgument), 
+     * 		   perche bisogna lavorare in coppia con 2 parametri (setRectangular, setPolar)
+     * 
+     * 		3) siccome avrei bisogno di 2 costruttori, che non possono esistere contemporaneamente 
+     * 		   dato che richiedono entrambia lo stesso numero di valori dello stesso tipo, non ne ho utilizzato nessuno
+     * 
+     * 		4) Devi usare 4 funzioni getter perche un metodo non puo ritornare 2 valori
+     *
+     *      5) 
+     *    		x = a1+b1i 
+     *      	y = a2+b2i
+     *      	x+y = (a1+a2)+(b1+b2)i
+     *      	x-y = (a1-a2)+(b1-b2)i
+     *      
+     *      6) 
+     *	    	x = a1+b1i 
+     *     		y = a2+b2i
+     *      	Argomento = ArgomentoX+ArgomentoY
+     *      	Modulo = ModuloX*ModuloY
+	 *	    	Argomento = ArgomentoX-ArgomentoY
+     *      	Modulo = ModuloX/ModuloY
+     *	
+     *	    7) 
+     *      	x = a+bi 
+     *      	x = a-bi
+     */
+     * 
+     *
+     * 
+     */
 }
--- src/ese102/Ese102.java
+++ src/ese102/Ese102.java
@@ -6,69 +6,111 @@ import java.io.*;
 
 public class Ese102
 {
-    public static void main(String[] args) throws IOException{
-    	
-    	ComplexNumber cn1 = new ComplexNumber();
-        ComplexNumber cn2 = new ComplexNumber();
-        InputStreamReader input = new InputStreamReader(System.in);
-    	BufferedReader tastiera = new BufferedReader(input);
-    	String riga;
-    	boolean valore=false;
-    	double leggiNumero=0;
-    	double leggiNumbero1=0;
-    	while(!valore){
-    		System.out.println("Coordinata X: ");
-    		riga = tastiera.readLine();
-    		try{
-    			leggiNumero = Double.valueOf(riga).doubleValue();
-    			valore = true;
-    		}
-    		catch(NumberFormatException e){
-    			System.out.println("sei down");
-    		}
+    public static void main(String[] args) throws IOException
+    {
+    	menu();
+    	ComplexNumber coordinata1 = new ComplexNumber(), coordinata2 = new ComplexNumber();
+    	boolean correctValue = false, loop = false;
+    	double leggiNumero = 0;
+    	byte scelta = 0;
+    	while(!loop)
+    	{
+    		scelta = 0;
+	        while(scelta < 1 || scelta > 7)
+	        {
+	        	System.out.print("Scelta: ");
+	        	scelta = (byte)readInput("", "hai inserito un numero errato");
+	        }
+	        if(scelta == 1)
+	        {
+	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	System.out.println("\n" + "Modulo: " + coordinata1.getModulus());
+	            System.out.println("Argomento: " + coordinata1.getArgument());       
+	        }
+	        else if(scelta == 2)
+	        {
+	        	leggiNumero = readInput("Inserisci l'argomento: ", "hai inserito un numero errato");
+	        	while(!correctValue)
+	        	{
+	        		try
+		        	{
+		        		coordinata1.setPolar(leggiNumero, readInput("Inserisci il Modulo( <= 0 ): ", "hai inserito un numero errato"));
+		        		correctValue = true;
+		        	}
+		        	catch (IllegalArgumentException e)
+		            {
+		                System.out.println("Il valore del modulo deve essere uguale o maggiore a 0");
+		            }
+	        	}
+	        	correctValue = false;
+	        	System.out.println("\n" + coordinata1.formatComplexNumber());
+	        }
+	        else if(scelta == 3)
+	        {
+	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	coordinata2.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	System.out.println(coordinata1.add(coordinata2).formatComplexNumber());
+	        }
+	        else if(scelta == 4)
+	        {
+	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	coordinata2.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	System.out.println(coordinata1.sub(coordinata2).formatComplexNumber());
+	        }
+	        else if(scelta == 5)
+	        {
+	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	coordinata2.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	System.out.println(coordinata1.multiply(coordinata2).formatComplexNumber());
+	        }
+	        else if(scelta == 6)
+	        {
+	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	coordinata2.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	System.out.println(coordinata1.divide(coordinata2).formatComplexNumber());
+	        }
+	        else if(scelta == 7)
+	        {
+	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
+	        	System.out.println(coordinata1.getConjugate().formatComplexNumber());
+	        }
+	        else
+	        	System.out.println("hai inserito un numero errato");
     	}
-    	valore = false;
-    	while(!valore){
-    		System.out.println("Coordinata Y: ");
-    		riga = tastiera.readLine();
-    		try{
-    			leggiNumbero1 = Double.valueOf(riga).doubleValue();
-    			valore = true;
-    		}
-    		catch(NumberFormatException e){
-    			System.out.println("sei ancora + down");
-    		}
-    	}
-    	valore = false;
-    	cn1.setRectangular(leggiNumero, leggiNumbero1);	
-    	while(!valore){
-    		System.out.println("Modulo: ");
-    		riga = tastiera.readLine();
-    		try{
-    			leggiNumero = Double.valueOf(riga).doubleValue();
-    			valore = true;
-    		}
-    		catch(NumberFormatException e){
-    			System.out.println("il ritardato dei ritardati che scrivi scemo?");
-    		}
-    	}
-    	valore = false;
-    	while(!valore){
-    		System.out.println("Argomento: ");
-    		riga = tastiera.readLine();
-    		try{
-    			leggiNumbero1 = Double.valueOf(riga).doubleValue();
-    			valore = true;
-    		}
-    		catch(NumberFormatException e){
-    			System.out.println("basta ci rinuncio");
-    		}
-    	}
-    	cn2.setPolar(leggiNumero, leggiNumbero1);
-    	System.out.println("Argomento: " + cn1.getArgument());
-        System.out.println("Modulo: " + cn1.getModulus());
-        System.out.println("Re: " + cn2.getRe());
-        System.out.println("Im: " + cn2.getIm());
     }
     
+    private static double readInput(String output, String error) throws IOException
+    {
+    	InputStreamReader input = new InputStreamReader(System.in);
+    	BufferedReader keyboard = new BufferedReader(input);
+    	String readInput;
+    	boolean correctValue = false;
+    	double readNumber = 0;
+        while(!correctValue)
+        {
+            System.out.println(output);
+            readInput = keyboard.readLine();
+            try
+            {
+            	readNumber = Double.valueOf(readInput).doubleValue();
+                correctValue = true;
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println(error);
+            }
+        }
+        return readNumber;
+    }
+    
+    private static void menu()
+    {
+    	System.out.println("[1] Da rettangolare a polare");
+    	System.out.println("[2] Da polare a rettangolare");
+    	System.out.println("[3] Somma due numeri complessi");
+    	System.out.println("[4] Sotrai due numeri complessi");
+    	System.out.println("[5] Moltiplica due numeri complessi");
+    	System.out.println("[6] Dividi due numeri complessi");
+    	System.out.println("[7] Ottieni il coniugato del numero complesso");
+    }
 }
