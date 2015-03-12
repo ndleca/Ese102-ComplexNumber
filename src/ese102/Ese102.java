02cef1ca1b3f05ceb7b6f0f419c63b5373d3c4f8
ndleca <andreileca54@yahoo.it>
2015-03-12 23:50:52 +0100

    Update Ese102.java

--- src/ese102/Ese102.java
+++ src/ese102/Ese102.java
@@ -1,116 +1,55 @@
 package ese102;
 
 import complexnumber.*;
-
-import java.io.*;
+import complexnumber.ComplexNumber.StringFormat;
 
 public class Ese102
 {
-    public static void main(String[] args) throws IOException
-    {
-    	menu();
-    	ComplexNumber coordinata1 = new ComplexNumber(), coordinata2 = new ComplexNumber();
-    	boolean correctValue = false, loop = false;
-    	double leggiNumero = 0;
-    	byte scelta = 0;
-    	while(!loop)
-    	{
-    		scelta = 0;
-	        while(scelta < 1 || scelta > 7)
-	        {
-	        	System.out.print("Scelta: ");
-	        	scelta = (byte)readInput("", "hai inserito un numero errato");
-	        }
-	        if(scelta == 1)
-	        {
-	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	System.out.println("\n" + "Modulo: " + coordinata1.getModulus());
-	            System.out.println("Argomento: " + coordinata1.getArgument());       
-	        }
-	        else if(scelta == 2)
-	        {
-	        	leggiNumero = readInput("Inserisci l'argomento: ", "hai inserito un numero errato");
-	        	while(!correctValue)
-	        	{
-	        		try
-		        	{
-		        		coordinata1.setPolar(leggiNumero, readInput("Inserisci il Modulo( <= 0 ): ", "hai inserito un numero errato"));
-		        		correctValue = true;
-		        	}
-		        	catch (IllegalArgumentException e)
-		            {
-		                System.out.println("Il valore del modulo deve essere uguale o maggiore a 0");
-		            }
-	        	}
-	        	correctValue = false;
-	        	System.out.println("\n" + coordinata1.formatComplexNumber());
-	        }
-	        else if(scelta == 3)
-	        {
-	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	coordinata2.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	System.out.println(coordinata1.add(coordinata2).formatComplexNumber());
-	        }
-	        else if(scelta == 4)
-	        {
-	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	coordinata2.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	System.out.println(coordinata1.sub(coordinata2).formatComplexNumber());
-	        }
-	        else if(scelta == 5)
-	        {
-	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	coordinata2.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	System.out.println(coordinata1.multiply(coordinata2).formatComplexNumber());
-	        }
-	        else if(scelta == 6)
-	        {
-	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	coordinata2.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	System.out.println(coordinata1.divide(coordinata2).formatComplexNumber());
-	        }
-	        else if(scelta == 7)
-	        {
-	        	coordinata1.setRectangular(readInput("Inserisci RE: ", "hai inserito un numero errato"), readInput("Inserisci IM: ", "hai inserito un numero errato"));
-	        	System.out.println(coordinata1.getConjugate().formatComplexNumber());
-	        }
-	        else
-	        	System.out.println("hai inserito un numero errato");
-    	}
-    }
-    
-    private static double readInput(String output, String error) throws IOException
-    {
-    	InputStreamReader input = new InputStreamReader(System.in);
-    	BufferedReader keyboard = new BufferedReader(input);
-    	String readInput;
-    	boolean correctValue = false;
-    	double readNumber = 0;
-        while(!correctValue)
-        {
-            System.out.println(output);
-            readInput = keyboard.readLine();
-            try
-            {
-            	readNumber = Double.valueOf(readInput).doubleValue();
-                correctValue = true;
-            }
-            catch (NumberFormatException e)
-            {
-                System.out.println(error);
-            }
-        }
-        return readNumber;
-    }
-    
-    private static void menu()
-    {
-    	System.out.println("[1] Da rettangolare a polare");
-    	System.out.println("[2] Da polare a rettangolare");
-    	System.out.println("[3] Somma due numeri complessi");
-    	System.out.println("[4] Sotrai due numeri complessi");
-    	System.out.println("[5] Moltiplica due numeri complessi");
-    	System.out.println("[6] Dividi due numeri complessi");
-    	System.out.println("[7] Ottieni il coniugato del numero complesso");
-    }
+	public static void main(String[] args)
+	{
+		ComplexNumber.setInitRectangular(0, 0);
+		ComplexNumber c1 = new ComplexNumber();
+		ComplexNumber.setInitRectangular(1, 1);
+		ComplexNumber c2 = new ComplexNumber();
+		System.out.println("c1: " + c1);
+		System.out.println("c1 (polar): " + c1.toString(StringFormat.POLAR));
+		System.out.println("c2: " + c2);
+		System.out.println("c2 (polar): " + c2.toString(StringFormat.POLAR));
+		System.out.println("Addiction: " + c1.add(c2));
+		System.out.println("Addiction (polar): " + (c1.add(c2)).toString(StringFormat.POLAR));
+		System.out.println("Subtraction: " + c1.sub(c2));
+		/* Domanda: come posso controllare il formato della
+		 * rappresentazione a stringa del risultato dell'operazione, senza dover assegnare
+		 * il risultato ad una variabile?
+		 */
+		System.out.println("Subtraction (polar): " + (c1.sub(c2)).toString(StringFormat.POLAR));
+		System.out.println("Multiplication: " + (c1.multiply(c2)).toString(StringFormat.RECTANGULAR));
+		System.out.println("Multiplication (polar): " + (c1.multiply(c2)).toString(StringFormat.POLAR));
+		System.out.println("Division: " + (c1.divide(c2)).toString(StringFormat.RECTANGULAR));
+		System.out.println("Division (polar): " + (c1.divide(c2)).toString(StringFormat.POLAR));
+		System.out.println("Complex Conjugate 1: " + (c1.getConjugate()).toString(StringFormat.RECTANGULAR));
+		System.out.println("Complex Conjugate 1 (polar): " + (c1.getConjugate()).toString(StringFormat.POLAR));
+		System.out.println("Complex Conjugate 2: " + (c2.getConjugate()).toString(StringFormat.RECTANGULAR));
+		System.out.println("Complex Conjugate 2 (polar): " + (c2.getConjugate()).toString(StringFormat.POLAR));
+		//----- Uguaglianza tra oggetti
+		ComplexNumber numeri[] = new ComplexNumber[4];
+		ComplexNumber.setInitRectangular(1, -2);
+		numeri[0] = new ComplexNumber();
+		numeri[1] = new ComplexNumber();
+		numeri[2] = numeri[1];
+		numeri[3] = new ComplexNumber();
+		numeri[3].setRectangular(-3, 2);
+		int i=0;
+		for(i=1; i<numeri.length; i++)
+		{
+			if(numeri[i-1] == numeri[i])
+				System.out.println("numeri[" + (i-1) + "] e numeri[" + i + "] si riferiscono alla medesima istanza");
+			else
+				System.out.println("numeri[" + (i-1) + "] e numeri[" + i + "] si riferiscono a due istanze differenti");		
+			if(numeri[i-1].equals(numeri[i]))
+				System.out.println("numeri[" + (i-1) + "] e numeri[" + i + "] contengono il medesimo numero complesso");
+			else
+				System.out.println("numeri[" + (i-1) + "] e numeri[" + i + "] contengono numeri complessi diversi");
+		}
+	}
 }
